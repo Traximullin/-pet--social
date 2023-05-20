840e7080079a1687730f27afc228a1cb38ec49d1
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-20 21:25:30 +0300

    input

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,12 @@
 import { type FC } from "react"
+import { Input } from "shared/ui"
 import "./index.scss"
 
 const App: FC = () => {
     return (
-        <div className="App">
-          Привет
-        </div>
+        <>
+            <Input />
+        </>
     )
 }
 
