253cf5dac4d4e911edf15d73bae0bda1c4542363
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-21 18:36:44 +0300

    button

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,12 +1,15 @@
 import { type FC } from "react"
 import { SignLayout } from "shared/layout"
-import { Form, Text } from "shared/ui"
+import { Button, Form, Text } from "shared/ui"
 
 const SignInPage: FC = () => {
     return (
         <SignLayout>
             <Form>
                 <Text.Header position="center">Войти</Text.Header>
+                <Button>
+                    Привет
+                </Button>
             </Form>
         </SignLayout>
     )
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Input } from "./Input"
 export { default as Form } from "./Form"
 export { default as Text } from "./Text"
+export { default as Button } from "./Button"
