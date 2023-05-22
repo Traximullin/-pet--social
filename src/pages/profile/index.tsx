5f0c536521353a898a1e9d9f872bb4b3abe00ed4
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-22 19:11:41 +0300

    space

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,12 +1,19 @@
 import { type FC } from "react"
 import { BaseLayout } from "shared/layout"
-import { Avatar, Content } from "shared/ui"
+import { Avatar, Content, Space } from "shared/ui"
 
 const ProfilePage: FC = () => {
     return (
         <BaseLayout>
             <Content>
-                <Avatar />
+                <Space gap="32px" alignItems="center">
+                    <Avatar />
+                    <div>
+                        <div>1</div>
+                        <div>1</div>
+                        <div>1</div>
+                    </div>
+                </Space>
             </Content>
         </BaseLayout>
     )
