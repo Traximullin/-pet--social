cf49cd90e939204437cfa004bbbc8842bfa572ce
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-05-27 13:25:30 +0300

    sign-up-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { type FC } from "react"
 import { Route, Routes } from "react-router-dom"
-import { EPage, HomePage, ProfilePage, SignInPage } from "./pages"
+import { EPage, HomePage, ProfilePage, SignInPage, SignUpPage } from "./pages"
 
 const Routing: FC = () => {
     return (
         <Routes>
             <Route path={EPage.HOME} element={<HomePage />} />
             <Route path={EPage.LOGIN} element={<SignInPage />} />
+            <Route path={EPage.REGISTATION} element={<SignUpPage />} />
             <Route path={EPage.PROFILE} element={<ProfilePage />} />
         </Routes>
     )
