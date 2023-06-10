6573e7bde2dbc0bd08b4a36a63a532730b43b518
Traximullin <66427615+Traximullin@users.noreply.github.com>
2023-06-10 21:45:35 +0300

    tree page

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -2,10 +2,12 @@ export { default as HomePage } from "./home"
 export { default as SignInPage } from "./sign-in"
 export { default as ProfilePage } from "./profile"
 export { default as SignUpPage } from "./sign-up"
+export { default as TreePage } from "./tree"
 
 export enum EPage {
     HOME = "/",
     LOGIN = "/login",
     REGISTATION = "/registration",
-    PROFILE = "/profile"
+    PROFILE = "/profile",
+    TREE = "/tree"
 }
---
+++ src/pages/tree/index.tsx
@@ -0,0 +1,12 @@
+import { type FC } from "react"
+import { BaseLayout } from "shared/layout"
+
+const TreePage: FC = () => {
+    return (
+        <BaseLayout>
+            Tree Page
+        </BaseLayout>
+    )
+}
+
+export default TreePage
