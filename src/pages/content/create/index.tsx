8fce2fa75abf6bf2ad7e54c62374b0b44b6c540d
traximullin <traximullin@bk.ru>
2023-06-25 21:22:37 +0300

    content-create

---
+++ src/pages/content/create/index.tsx
@@ -0,0 +1,13 @@
+import { type FC } from "react"
+import { BaseLayout } from "shared/layout"
+
+const ContentCreate: FC = () => {
+
+    return (
+        <BaseLayout>
+            Content Create
+        </BaseLayout>
+    )
+}
+
+export default ContentCreate
