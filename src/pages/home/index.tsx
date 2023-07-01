408e6f9c556828c0614ca9db610acd976e593d94
traximullin <traximullin@bk.ru>
2023-07-01 21:27:38 +0300

    comment

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,11 @@
 import { TapeCard } from "entities/tape/ui"
 import { type FC } from "react"
 import { BaseLayout } from "shared/layout"
-import { List, Popup } from "shared/ui"
+import { List } from "shared/ui"
 
 const HomePage: FC = () => {
     return (
         <BaseLayout>
-            <Popup />
             <List
                 data={[1, 2, 3, 4]}
                 renderItem={(item) => (
