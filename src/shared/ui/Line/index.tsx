6f0893a875ae410091e454201bda50f33c7e325c
traximullin <traximullin@bk.ru>
2023-07-09 21:12:54 +0300

    line

---
+++ src/shared/ui/Line/index.tsx
@@ -0,0 +1,12 @@
+import { type FC } from "react"
+import "./index.scss"
+import { type ILine } from "./interface"
+
+const Line: FC<ILine> = () => {
+
+    return (
+        <div className="line" />
+    )
+}
+
+export default Line
---
+++ src/shared/ui/Line/interface.ts
@@ -0,0 +1,5 @@
+
+export interface ILine {
+    to?: string
+    from?: string
+}
