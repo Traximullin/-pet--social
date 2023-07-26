8ed6671aacac7a31614ebb306e6c63d05fd962f3
traximullin <traximullin@bk.ru>
2023-07-26 21:08:18 +0300

    useTitle

---
+++ src/shared/hooks/useTitle/index.ts
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+
+const useTitle = (title: string): void => {
+
+    useEffect(() => {
+        document.title = title
+    }, [title])
+
+}
+
+export default useTitle
