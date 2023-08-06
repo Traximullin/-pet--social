b07e3e8f6213374e1f0e6b5a70a702f5a6cbdc84
traximullin <traximullin@bk.ru>
2023-08-06 21:21:04 +0300

    Horizontal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,17 @@
 import { TapeCard } from "entities/tape/ui"
 import { type FC } from "react"
 import { BaseLayout } from "shared/layout"
-import { List } from "shared/ui"
+import { Horizontal, List } from "shared/ui"
 
 const HomePage: FC = () => {
     return (
         <BaseLayout>
+            <Horizontal>
+                <div>1</div>
+                <div>1</div>
+                <div>1</div>
+                <div>1</div>
+            </Horizontal>
             <List
                 data={[1, 2, 3, 4]}
                 renderItem={(item) => (
---
+++ src/shared/ui/Horizontal/interface.ts
@@ -0,0 +1,5 @@
+import { type HTMLAttributes } from "react"
+
+export interface IHorizontal extends HTMLAttributes<HTMLDivElement> {
+
+}
