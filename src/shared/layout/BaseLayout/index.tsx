9ef25c297755136bae64a605829be60e55cf95c6
traximullin <traximullin@bk.ru>
2023-08-19 17:04:14 +0300

    circle-layout

--- src/shared/layout/BaseLayout/index.tsx
+++ src/shared/layout/BaseLayout/index.tsx
@@ -2,12 +2,14 @@ import { type FC } from "react"
 import { Menu } from "shared/ui"
 import "./index.scss"
 import { type IBaseLayout } from "./interface"
+import CircleLayout from "../CircleLayout"
 
 const BaseLayout: FC<IBaseLayout> = (props) => {
     const { children, ...othersProps } = props
 
     return (
         <main className="base-layout" {...othersProps}>
+            <CircleLayout />
             <div className="base-layout__content">
                 {children}
             </div>
