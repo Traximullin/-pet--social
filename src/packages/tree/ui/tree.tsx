f777afb7d00dff8dff0e868243e92c6f196d0f20
traximullin <traximullin@bk.ru>
2023-09-03 17:58:12 +0300

    packages-tree

--- src/packages/tree/ui/tree.tsx
+++ src/packages/tree/ui/tree.tsx
@@ -1,24 +1,58 @@
-import { useRef, type FC, useEffect } from "react"
+import { type FC, useEffect, useRef } from "react"
+import * as d3 from "d3"
 
-const Tree: FC = () => {
-    const canvasRef = useRef<HTMLCanvasElement | null>(null)
-
-    useEffect(() => {
-        if (!canvasRef.current) return
-
-        const canvas = canvasRef.current
-        const context = canvas.getContext("2d")
-
-        if (!context) return
-
-        canvas.width = window.innerWidth
-        canvas.height = window.innerHeight
-
-        context.fillStyle = "black"
-        context.fillRect(0, 0, canvas.width, canvas.height)
-    }, [])
-
-    return <canvas ref={canvasRef} />
+interface FamilyMember {
+  id: number
+  name: string
+  x: number // Тип для координаты x
+  y: number // Тип для координаты y
+  // Другие поля для членов семьи
 }
 
-export { Tree }
+interface LinkData {
+  source: FamilyMember
+  target: FamilyMember
+  // Другие поля для связей
+}
+
+interface FamilyTreeProps {
+  data: FamilyMember[]
+  linksData: LinkData[]
+}
+
+const Tree: FC<FamilyTreeProps> = (props) => {
+    const { data, linksData, } = props
+    const svgRef = useRef<SVGSVGElement | null>(null)
+
+    useEffect(() => {
+        const svg = d3.select(svgRef.current)
+
+        const nodes = svg.selectAll<SVGCircleElement, FamilyMember>("circle").data(data)
+        nodes
+            .enter()
+            .append<SVGCircleElement>("circle")
+            .attr("r", 20)
+            // @ts-expect-error
+            .attr("cx", (d) => d.x)
+            // @ts-expect-error
+            .attr("cy", (d) => d.y)
+
+        const links = svg.selectAll<SVGLineElement, LinkData>("line").data(linksData)
+        links
+            .enter()
+            .append<SVGLineElement>("line")
+            .attr("x1", (d) => d.source.x)
+            .attr("y1", (d) => d.source.y)
+            .attr("x2", (d) => d.target.x)
+            .attr("y2", (d) => d.target.y)
+
+    }, [data, linksData])
+
+    return (
+        <div className="family-tree">
+            <svg ref={svgRef}></svg>
+        </div>
+    )
+}
+
+export default Tree
