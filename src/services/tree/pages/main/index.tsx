37db7b7296b4d36d54b06213fbd669272feb2d7c
traximullin <traximullin@bk.ru>
2023-11-25 21:34:27 +0300

    Компонент дерева

--- src/services/tree/pages/main/index.tsx
+++ src/services/tree/pages/main/index.tsx
@@ -3,23 +3,10 @@ import { BaseLayout } from "packages/ui-kit/layout"
 import { type FC } from "react"
 
 const TreePage: FC = () => {
-    const familyData = {
-        id: 1,
-        name: "John",
-        spouseId: 2,
-        children: [3, 4],
-    }
-
-    const familyMembers = {
-        1: { id: 1, name: "John", spouseId: 2, children: [3, 4], },
-        2: { id: 2, name: "Mary", spouseId: 1, children: [3, 4], },
-        3: { id: 3, name: "Alice", parents: [1, 2], },
-        4: { id: 4, name: "Bob", parents: [1, 2], },
-    }
 
     return (
         <BaseLayout>
-            <Tree data={[]} linksData={[]} />
+            <Tree/>
         </BaseLayout>
     )
 }
